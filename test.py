4e0dd14eba2291c0388edc79d5192c849de4a929
Aaditya Chaudhari <115763672+aadi1720@users.noreply.github.com>
2023-10-17 21:20:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,104 +1,49 @@
+# test_bank_system.py
+
 import unittest
-import io
-import sys
-from unittest.mock import patch
+from bank_system import BankAccount
+from bank_exceptions import InsufficientFundsError, NegativeAmountError
 
-from contacts import *
+class TestBankAccount(unittest.TestCase):
 
-import os
-def remove_file(filename):
-    if os.path.exists(filename):
-        os.remove(filename)
+    def setUp(self):
+        self.account = BankAccount(101, "Alice", 1000)
 
-class Test01_AddContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test01 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: contacts.add_contact(id = '7145551212', first_name = 'Richard', last_name = 'Stallman') *** EXPECT: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates'], '7145551212': ['Richard', 'Stallman']} ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.add_contact(id = '7145551212', first_name = 'Richard', last_name = 'Stallman')
-        c = {}
-        with open('t99.dat', 'r') as f:
-            c = json.load(f)
-        self.assertEqual(c, {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates'], '7145551212': ['Richard', 'Stallman']})
-        remove_file('t99.dat')
-        print()
+    def test_deposit_positive_amount(self):
+        result = self.account.deposit(500)
+        # Round to 2 decimal places for comparison
+        result = round(float(result.split("$")[-1].split(" ")[0]), 2)
+        expected = round(1500.0, 2)
+        self.assertEqual(result, expected)
 
-class Test02_AddContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test02 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: r = contacts.add_contact(id = '7145551212', first_name = 'Richard', last_name = 'Stallman') *** EXPECT: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates'], '7145551212': ['Richard', 'Stallman']} ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.add_contact(id = '7145551111', first_name = 'Richard', last_name = 'Stallman')
-        self.assertEqual(r, 'error')
-        remove_file('t99.dat')
-        print()
+    def test_deposit_negative_amount(self):
+        with self.assertRaises(NegativeAmountError) as context:
+            self.account.deposit(-200)
+        self.assertEqual(str(context.exception), "Amount cannot be negative: $-200")
 
-class Test03_ModifyContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test03 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: contacts.modify_contact(id = '7145551111', first_name = 'Richard', last_name = 'Stallman') *** EXPECT: filename t99.dat = {'5625553333': ['Bill', 'Gates'], '7145551111': ['Richard', 'Stallman']} ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.modify_contact(id = '7145551111', first_name = 'Richard', last_name = 'Stallman')
-        c = {}
-        with open('t99.dat', 'r') as f:
-            c = json.load(f)
-        self.assertEqual(c, {'5625553333': ['Bill', 'Gates'], '7145551111': ['Richard', 'Stallman']})
-        remove_file('t99.dat')
-        print()
+    def test_withdraw_positive_amount(self):
+        result = self.account.withdraw(200)
+        # Round to 2 decimal places for comparison
+        result = round(float(result.split("$")[-1].split(" ")[0]), 2)
+        expected = round(800.0, 2)
+        self.assertEqual(result, expected)
 
-class Test04_ModifyContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test04 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: r = contacts.modify_contact(id = '7145559999', first_name = 'Richard', last_name = 'Stallman') *** EXPECT: r = 'error' ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.modify_contact(id = '7145559999', first_name = 'Richard', last_name = 'Stallman')
-        self.assertEqual(r, 'error')
-        remove_file('t99.dat')
-        print()
+    def test_get_balance(self):
+        result = self.account.get_balance()
+        # Round to 2 decimal places for comparison
+        result = round(float(result.split("$")[-1].split(" ")[0]), 2)
+        expected = round(1000.0, 2)
+        self.assertEqual(result, expected)
 
-class Test05_DeleteContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test05 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: contacts.delete_contact(id = '7145551111') *** EXPECT: filename t99.dat = {'5625553333': ['Bill', 'Gates']} ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.delete_contact(id = '7145551111')
-        c = {}
-        with open('t99.dat', 'r') as f:
-            c = json.load(f)
-        self.assertEqual(c, {'5625553333': ['Bill', 'Gates']})
-        remove_file('t99.dat')
-        print()
+    def test_withdraw_insufficient_funds(self):
+        with self.assertRaises(InsufficientFundsError) as context:
+            self.account.withdraw(2000)
+        self.assertEqual(str(context.exception), "Insufficient funds. You tried to withdraw $2000, but your balance is only $1000.")
 
-class Test06_DeleteContact(unittest.TestCase):
-    def test_list_int(self):
-        """
-        *** Test06 *** GIVEN: filename t99.dat = {'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']} AND contacts = Contacts(filename='t99.dat') *** FUNCTION CALL: r = contacts.delete_contact(id = '7145551111') *** EXPECT: r = 'error' ***
-        """
-        with open('t99.dat', 'w') as f:
-            json.dump({'7145551111': ['Steve', 'Jobs'], '5625553333': ['Bill', 'Gates']}, f)
-        contacts = Contacts(filename='t99.dat')
-        r = contacts.delete_contact(id = '7145559999')
-        self.assertEqual(r, 'error')
-        remove_file('t99.dat')
-        print()
-
+    def test_withdraw_negative_amount(self):
+        with self.assertRaises(NegativeAmountError) as context:
+            self.account.withdraw(-200)
+        self.assertEqual(str(context.exception), "Amount cannot be negative: $-200")
 
 if __name__ == '__main__':
-    with open('test.txt', "w") as f:
-        runner = unittest.TextTestRunner(f)
-        unittest.main(testRunner=runner)
+    unittest.main()
